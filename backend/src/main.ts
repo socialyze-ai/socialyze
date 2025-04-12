ba6e2647a3d619229b026ddfbea1da93d7198a8e
Akash Dutta <akash.dutta9830489799@gmail.com>
2025-04-12 18:20:09 +0530

    Deploy test

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,7 +7,11 @@ async function bootstrap() {
   app.use(cookieParser());
 
   app.enableCors({
-    origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+    origin: [
+      'http://localhost:5173',
+      'http://127.0.0.1:5173',
+      'https://socialyze-frontend.netlify.app',
+    ],
     credentials: true,
     allowedHeaders: 'Content-Type, Authorization, Set-Cookie',
     exposedHeaders: 'Set-Cookie',
