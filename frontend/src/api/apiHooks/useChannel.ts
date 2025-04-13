0858c8173c3cb1e8e5d726584fed89993df48ca9
mrpp2001 <pratiknpatil2001@gmail.com>
2025-04-13 21:00:09 +0530

    Refactor channel API hooks and update backend URL
    - Renamed `useChannel` to `useChannelAuth` and `useMessageListener` to `useGetChannel` for clarity.
    - Updated the backend URL to point to the staging environment.
    - Cleaned up code formatting for better readability in `Channels.tsx`.

--- frontend/src/api/apiHooks/useChannel.ts
+++ frontend/src/api/apiHooks/useChannel.ts
@@ -2,30 +2,25 @@ import { useMutation, useQuery } from "@tanstack/react-query";
 import { HARD_CODED_TOKEN, makeRequest } from "./utils";
 import { BACKEND_URL } from "@/config/config";
 
-export const useChannel = () => {
+export const useChannelAuth = () => {
   return useMutation({
     mutationFn: async (body: { handle: string }) => {
       const { data } = await makeRequest(
         BACKEND_URL + "channel/getAuthUrl",
         "POST",
         body,
-        HARD_CODED_TOKEN
+        HARD_CODED_TOKEN,
       );
       return data;
     },
   });
 };
 
-export const useMessageListener = () => {
+export const useGetChannel = () => {
   const query = useQuery({
     queryKey: ["channels"],
     queryFn: async () => {
-      return await makeRequest(
-        BACKEND_URL + "channel",
-        "GET",
-        "",
-        HARD_CODED_TOKEN
-      );
+      return await makeRequest(BACKEND_URL + "channel", "GET", "", HARD_CODED_TOKEN);
     },
   });
 
