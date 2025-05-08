448ed2b9f20dd65b399430b385d4d05ad61e3c39
Akash Dutta <akash.dutta9830489799@gmail.com>
2025-05-09 02:07:11 +0530

    Blank page issue

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,6 +9,9 @@ export default defineConfig(({}) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  optimizeDeps: {
+    include: ["react", "react-dom"],
+  },
   build: {
     chunkSizeWarningLimit: 1000,
     rollupOptions: {
