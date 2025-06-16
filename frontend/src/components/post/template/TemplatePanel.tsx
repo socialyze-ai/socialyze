027083784b9395ba60e0e66265aab2bd52fad1f8
mrpp2001 <pratiknpatil2001@gmail.com>
2025-06-16 21:54:06 +0530

    Refactor TemplatePanel for improved template category handling
    - Updated logic to fetch post templates based on active suggestions and selected categories, enhancing clarity and accuracy in template retrieval.
    - Simplified category selection process by directly referencing active suggestions, improving code maintainability and user experience.

--- frontend/src/components/post/template/TemplatePanel.tsx
+++ frontend/src/components/post/template/TemplatePanel.tsx
@@ -65,15 +65,19 @@ const TemplatePanel = () => {
 
   const { data: postTemplatesDefault, isPending: isLoadingDefault } = useGetPostTemplatesDefault({
     postCategory:
-      postCreation.selectedTemplateCategory?.name === SPECIAL_TEMPLATES.grid
+      activeSuggestion[activeTab] === SPECIAL_TEMPLATES.grid
         ? SPECIAL_TEMPLATES.grid
-        : postCreation.selectedTemplateCategory?.name === SPECIAL_TEMPLATES.carousel
+        : activeSuggestion[activeTab] === SPECIAL_TEMPLATES.carousel
         ? SPECIAL_TEMPLATES.carousel
-        : postCreation.selectedTemplateCategory?._id,
+        : categories[activeTab]?.find((cat) => cat.name === activeSuggestion[activeTab])?._id,
   });
 
   const { data: postTemplatesCustom, isPending: isLoadingCustom } = useGetPostTemplatesCustom({
-    postCategory: postCreation.selectedTemplateCategory?._id,
+    postCategory:
+      activeSuggestion[activeTab] === SPECIAL_TEMPLATES.grid ||
+      activeSuggestion[activeTab] === SPECIAL_TEMPLATES.carousel
+        ? activeSuggestion[activeTab]
+        : categories[activeTab]?.find((cat) => cat.name === activeSuggestion[activeTab])?._id,
   });
 
   console.log("postTemplatesDefault", postTemplatesDefault);
